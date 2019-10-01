d389d2b23fb3fbb69ae0046d7808591803fc318f
SerhiiPodaiko <podajko888@gmail.com>
2019-10-02 00:09:39 +0300

    Appo project

---
+++ js/script.js
@@ -0,0 +1,22 @@
+$( document ).ready(function() {
+    //back-to-top
+    let btnTop = $(".back-to-top")
+    $(window).on("scroll", function () {
+        if($(window).scrollTop() >= 300){
+            btnTop.fadeIn();
+        }else {
+            btnTop.fadeOut();
+        }
+    });
+
+    btnTop.on("click", function () {
+        $("html,body").animate({scrollTop:0},900)
+    });
+
+    //scroll-bar
+    $(window).scroll(function () {
+        let ratio = $(document).scrollTop() / (($(document).height() - $(window).height()) / 100);
+        $("#progress-bar").width(ratio + '%');
+    });
+
+});
